d4eb55b3083ba539645301aec72424e3c98e17e3
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-10-17 19:35:03 -0400

    add edges to graph

--- sudoku.py
+++ sudoku.py
@@ -5,20 +5,34 @@ Python code associated with graph-coloring based
 solution to a sudoku puzzle.
 """
 
-import networkx 
+import networkx
+import matplotlib.pyplot as plt
 
-def blank_graph_edges(dim):
+def blank_graph_edges(graph, dim):
     """
     Returns edges for a blank graph based on the dimension.
     """
-    pass
+    for x in range(1,13,4):
+        for i in range (x,x+2):
+            for j in range(i+1,i+3):
+                graph.add_edge(i,j)
+
+    for x in range (1,4):
+        for i in range(x,x+8,4):
+            for j in range(i+4,i+12,4)
+                graph.add_edge(i,j)
+
+    add_edges_from([(1,6), (2, 5), (3, 😎, (4, 7), (9, 14), (10, 13), (11, 16), (12, 15)])
+    add_edges_from([(1,6), (2, 5), (3, 8 ), (4, 7), (9, 14), (10, 13), (11, 16), (12, 15)])
+    return graph
 
 def create_blank_graph(dim):
     """
     Returns blank sudoku graph of specified dimensions.
-    Dimensions are for the ENTIRE row/column, not a 
+    Dimensions are for the ENTIRE row/column, not a
     single box (single box dimensios is sqrt(dim).
     """
     graph = networkx.Graph()
     graph.add_nodes_from(range(1, dim**2 + 1))
     graph.add_edges_from(blank_graph_edges(dim))
+    return graph
